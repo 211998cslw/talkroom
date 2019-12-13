ad9f2cc18eefebb84b37b3ccd50dd0314d8d2540
2926309810@qq.com <2926308910@qq.com>
2019-12-13 19:48:56 +0800

    aa

--- server.php
+++ server.php
@@ -9,11 +9,12 @@ $server->on('open', function (Swoole\WebSocket\Server $server, $request) {
 });
 
 $server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
-        // $data=$frame->data;
-        $info=json_decode($frame->data,true);
-        print_r($info);
+    $info=json_decode($frame->data,true);
+    if($info['type']=='login'){
+        $content="<p>系统消息：尊贵的用户".$info['content']."上线了</p>";
+    }
         $user=$frame->fd;
-        $content="<span style='clear:both'></span><p style='float:right'>".$frame->$data."</p>";
+        // $content="<span style='clear:both'></span><p style='float:right'>".$frame->$data."</p>";
         // $content=json_decode($data)['content'];
         // var_dump($data);
     // echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
@@ -27,3 +28,96 @@ $server->on('close', function ($ser, $fd) {
 $server->start();
 
  ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php
+$server = new Swoole\WebSocket\Server("0.0.0.0",9501);
+$server->on('open', function (Swoole\WebSocket\Server $server, $request) {
+});
+$server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+//    $data = $frame->data;
+    $info =json_decode($frame->data,true);
+    //var_dump($frame->data);
+    $key ="online_list";
+    $redis =new Redis();
+    $redis->connect('localhost',6379);
+    $liststr = $redis->get($key);
+    $list =json_decode($liststr,true);
+    //var_dump($list);
+
+    if   ($info['type']=='login'){
+        if(empty($list)){
+            $list =[];
+        }
+        $list[] =[
+            'client_id'=>$frame->fd,
+            'name'=>$info['content']
+        ];
+        $str =json_encode($list);
+        $redis->set($key,$str);
+        $content="<p align='center'>系统消息：尊贵的用户".$info['content']."进入聊天室</p>";
+    }else{
+        $content="<p align='right'><span style='border:1px solid #3f9ae5;margin-right:100px;background-color: #3c763d; width: auto;border-radius:20px;'>".$info['content']."</span></p>";
+    }
+//    $content = "<span style='clear: both'></span><p style='float:right'>".$frame->data."</p>";
+    //echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    foreach($list as $k=>$v){
+        $server->push($v['client_id'],$content);
+    }
+});
+$server->on('close', function ($user, $fd) {
+    $key ="online_list";
+    $redis =new Redis();
+    $redis->connet('localhost',6379);
+    $liststr = $redis->get($key);
+    $list =json_decode($liststr,true);
+    foreach ($list as $key=>$value){
+        if($value['client_id'] ==$fd){
+            unset($list[$key]);
+        }
+    }
+    $str =json_encode($list);
+    $redis->set($key,$str);
+    foreach ($list as $k=>$v){
+        $content="<p align='center'>系统消息：尊贵的用户".$v['name']."离开聊天室</p>";
+        $server->push($v['client_id'],$content);
+    }
+});
+$server->start();
--- talkroom.php
+++ talkroom.php
@@ -1,45 +1,48 @@
-<?php 
+<?php
 session_start();
-$id=$_GET['id'];
-// echo $_SESSION[$id]; 
- ?>
+$id = $_GET['id'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title></title>
+    <meta charset="UTF-8">
+    <title>直播</title>
 </head>
+<style>
+
+
+    font{
+        background: linear-gradient(to right, red, blue);
+        -webkit-background-clip: text;
+        color: transparent;
+</style>
 <body>
-	<div style="border:1px solid black; width:300px;height:200px;" id="message"></div>
+<marquee><font size=+3 color=red>仙女聊天室</font></marquee>
+<div style="border:1px solid black;width:700px;height:600px;overflow-y: auto" id="message"></div>
 <div>
-	<input type="text"  id="content">
-	<input type="button" value="发送" id="btn">
+    <input type="text" name="content" id="content">
+    <input type="button" value="发送" id="btn">
 </div>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+</body>
+<script src="jquery-3.3.1.js"></script>
 <script>
     if(window.WebSocket){
-        var ws =new WebSocket('ws://39.105.42.250:9501');
-        ws.onopen=function(event){
-            var username="<?php echo $_SESSION[$id];?>";
-            // alert(username);
-            var json='{"type":"login","content":"'+name+'"}';
-            // console.log(json);
+        var ws = new WebSocket('ws://39.105.42.250:9501');
+        ws.onopen =function(event){
+            var username ="<?php echo $_SESSION[$uid];?>";
+            console.log(username);
+            var json = '{"type":"login","content":"'+name+'"}';
             ws.send(json);
         }
-        ws.onmessage =function(event){
-            var msg=event.data;
-            console.log(msg);
-            $("#message").append("<p>"+msg+"</p >");
+        ws.onmessage = function(event){
+            var msg = event.data;
+            $("#message").append(msg);
         }
         $(document).on('click',"#btn",function(){
             var content = $('#content').val();
-            var json ="{'content':"+content+"}";
-            // alert(json);
-            ws.send(content);
+            var json = '{"type":"talk","content":"'+content+'"}';
+            ws.send(json);
         })
     }
-
-
 </script>
-</body>
 </html>
